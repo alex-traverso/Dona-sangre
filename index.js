8c47511a97b488648c20b656f5a0ff2871bb4a51
alex traverso <alextraverso6@gmail.com>
2022-11-13 16:45:00 -0300

    unified styles + sections

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-$(function() {
-	$('.toggler').on('click', function() {
-		$('nav').slideToggle(500);
-});
-});
+$( '.toggler' ).on( 'click', function () {
+	$( 'nav' ).toggle( 400 );
+} );
+
+// $('.faq-arrow').on('click', function() {
+// 	$( '.faq-item-info' ).toggle(400); 
+// 	
+// } ); 
+
+$( ".faq-item" ).on( "click", function () {
+	$( this ).find( ".faq-item-info" ).fadeToggle( 400 );
+	$( this ).find( '.faq-arrow-up' ).toggle();
+	$( this ).find( '.faq-arrow-down' ).toggle();
+} );
+
+//toggleClass("nombredeclase");
