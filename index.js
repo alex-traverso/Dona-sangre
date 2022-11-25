5b37fd69b3537613e9d27a9b13f503365f7d264e
alex traverso <alextraverso6@gmail.com>
2022-11-25 12:11:27 -0300

    fix faq arrow animation

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+/* CARROUSELL */
+import Glide from '@glidejs/glide';
+new Glide( '.glide' ).mount();
+
+/* FUNCION TOGGLE NAVBAR */
 $( '.toggler' ).on( 'click', function () {
 	$( 'nav' ).toggle( 400 );
 } );
 
-// $('.faq-arrow').on('click', function() {
-// 	$( '.faq-item-info' ).toggle(400); 
-// 	
-// } ); 
-
+/* FUNCION TOGGLE FAQ */
 $( ".faq-item" ).on( "click", function () {
 	$( this ).find( ".faq-item-info" ).fadeToggle( 400 );
-	$( this ).find( '.faq-arrow-up' ).toggle();
-	$( this ).find( '.faq-arrow-down' ).toggle();
+	$(this ).find( '.faq-arrow' ).toggleClass('rotate-arrow');
 } );
-
-//toggleClass("nombredeclase");
